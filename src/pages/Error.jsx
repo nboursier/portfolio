ab8786ebf6ba8af69bd662facfbfcb4c76deb16a
nboursier <nicolasboursier@yahoo.fr>
2025-05-26 17:12:47 +0200

    modif balise meta

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,20 +1,20 @@
 // import * as React from 'react';
 import { Link } from 'react-router-dom';
-// import Header from '../components/header';
-// import Footer from '../components/footer';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import '../scss/style.scss'
 import '../scss/pages/_error.scss';
 
 function Error() {
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
             <div className='error'>
                 <h2 className='error_title'>404</h2>
                 <p className='error_text'> Oups ! La page que vous demandez n&rsquo;existe pas.</p>
                 <Link className='error_return' to="/"> Retourner sur la page d&rsquo;accueil</Link>
             </div>
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 }
